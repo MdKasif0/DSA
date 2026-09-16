e99d4a01634d043e0514d70caee3434820a474b7
Md Kasif Uddin <mdkasifuddin123@gmail.com>
2026-09-16 17:09:11 +0530

    feat: add user input array C programs

---
+++ Array/User input array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+
+  int size;
+  printf("Enter the size of the array: ");
+  scanf("%d", &size);
+  int array[size];
+
+  for (int i = 0; i < size; i++) {
+    printf("Enter the %d element of the array: ", i);
+    scanf("%d", &array[i]);
+  }
+  printf("The array is: ");
+  for (int i = 0; i < size; i++) {
+    printf("%d ", array[i]);
+  }
+}
