22214d243c7705d80bbfd2f66017f83a2019fd72
Md Kasif Uddin <mdkasifuddin123@gmail.com>
2026-09-17 18:07:53 +0530

    fix: correct loop condition in Running Sum program to include all elements

--- Array/Running Sum — Cumulative Processing.c
+++ Array/Running Sum — Cumulative Processing.c
@@ -32,7 +32,7 @@ int main() {
 
     int sum=0;
     printf("The array is: ");
-    for (int i = 0; i < size-1; i++) {
+    for (int i = 0; i < size; i++) {
         sum += array[i];
         printf("%d ", sum);
     }
